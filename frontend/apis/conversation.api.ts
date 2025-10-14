eeaf5802943ce2a06a670d99438514abb3a8c4eb
Harsh-patel29 <harshbharat55@gmail.com>
2025-10-14 13:57:15 +0530

    Add conversation routes and API functions for fetching messages and conversations

---
+++ frontend/apis/conversation.api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import * as dotenv from "dotenv";
+
+export const getMessages = async (cursor: string | null, convId: string) => {
+  const result = await axios.get(
+    `${
+      process.env.NEXT_PUBLIC_API_URL
+    }/api/v1/conversation/messages?convId=${convId}${
+      cursor ? `&cursor=${cursor}` : ""
+    }`,
+    { withCredentials: true }
+  );
+  return result.data;
+};
+
+export const getConversations = async (cursor: string | null) => {
+  const result = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/conversation/fetchConversation${
+      cursor ? `?cursor=${cursor}` : ""
+    }`,
+    { withCredentials: true }
+  );
+  return result.data;
+};
