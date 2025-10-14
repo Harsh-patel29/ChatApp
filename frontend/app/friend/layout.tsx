64b13cfd3f39e3f161f5399ef96ccbf80e5b409e
Harsh-patel29 <harshbharat55@gmail.com>
2025-10-14 14:12:09 +0530

    Remove unused Friends and Friend Demo pages, along with layout component

--- frontend/app/friend/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from "react";
-
-const ChatLayout = ({ children }: { children: ReactNode }) => {
-  return <main className="h-screen">{children}</main>;
-};
-
-export default ChatLayout;
