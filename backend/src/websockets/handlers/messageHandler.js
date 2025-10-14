966ae2ee74a3fbf205cafa33f557eb05d4a24a51
Harsh-patel29 <harshbharat55@gmail.com>
2025-10-14 21:16:25 +0530

    Integrate Kafka producer in message handling and refine typing log output

--- backend/src/websockets/handlers/messageHandler.js
+++ backend/src/websockets/handlers/messageHandler.js
@@ -1,3 +1,4 @@
+import { checkAndFlush } from "../../kafka/producer.js";
 import { client } from "../../redis/client.js";
 
 export class SendMessage {
@@ -9,16 +10,19 @@ export class SendMessage {
         senderId,
         content,
         messageType,
+        timeStamp: Date.now(),
       };
       console.log("📩 New Message:", message);
-      const key = `chat:${conversationId}:messages`;
+
+      const key = `chat:${conversationId}:message`;
+
       await client.rpush(key, JSON.stringify(message));
-      // Optional: emit message back to all clients in the room
-      // socket.to(conversationId).emit("messageReceived", message);
-      /*
-       Todo:-
-       Add the kafka producer and TTL in redis 
-        */
+
+      socket.to(conversationId).emit("messageReceived", message);
+
+      checkAndFlush(conversationId).catch((err) =>
+        console.error("Error in checkAndFlush:", err)
+      );
     } catch (error) {
       console.error("❌ Error saving message in redis:", err);
     }
