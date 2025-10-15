859edb0cf64d3b80986f61179e3388d41771143f
Harsh-patel29 <harshbharat55@gmail.com>
2025-10-15 22:28:53 +0530

    Add Redis integration for message fetching and update API endpoints

--- backend/src/controller/conversation.controller.js
+++ backend/src/controller/conversation.controller.js
@@ -2,6 +2,7 @@ import { AsyncHandler } from "../utils/AsyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { PrismaClient } from "@prisma/client";
+import { client } from "../redis/client.js";
 
 const prisma = new PrismaClient();
 
@@ -24,7 +25,6 @@ export const fetchConversation = AsyncHandler(async (req, res) => {
       },
       orderBy: { id: "desc" },
     });
-    console.log(allMessages);
     const nextCursor =
       allMessages.length === limit
         ? allMessages[allMessages.length - 1].id
@@ -85,5 +85,47 @@ export const fetchConversationParticipants = AsyncHandler(async (req, res) => {
     );
   } catch (error) {
     throw new ApiError(500, "Error fetching Conversations", error);
+  }
+});
+
+export const getMessages = AsyncHandler(async (req, res) => {
+  try {
+    const cursor = req.query.cursor;
+    const conversationId = req.query.conversationId;
+
+    const key = `chat:${conversationId}:message`;
+
+    const redisMessage = await client.lrange(key, 0, -1);
+    const parsedRedisMessage = redisMessage.map((m) => JSON.parse(m));
+
+    const limit = 10;
+
+    const messages = await prisma.message.findMany({
+      take: limit,
+      where: { conversationId: conversationId },
+      ...(cursor &&
+        cursor != "undefined" && {
+          skip: 1,
+          cursor: { id: cursor },
+        }),
+      orderBy: { createdAt: "asc" },
+    });
+
+    const allMessages = [...messages, ...parsedRedisMessage].sort(
+      (a, b) =>
+        new Date(a.createdAt || a.timeStamp) -
+        new Date(b.createdAt || b.timeStamp)
+    );
+
+    const nextCursor =
+      messages.length === limit ? messages[messages.length - 1].id : null;
+    return res.status(200).json(
+      new ApiResponse(200, "Messages fetched Successfully", {
+        allMessages,
+        nextCursor,
+      })
+    );
+  } catch (error) {
+    throw new ApiError(500, "Error Fetching Messages", error);
   }
 });
